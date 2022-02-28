75d28106dcb72495f8c7ae77742544d98fc2cc15
Diego Botelho de Souza <89199976+diegodesouza17@users.noreply.github.com>
2022-02-28 14:02:29 -0300

    chore: first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
@@ -16,6 +15,20 @@ function checksExistsUserAccount(request, response, next) {
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name, username } = request.body
+  
+  const usernameAlreadyExists = users.some((user) => user.username === username)
+    if (usernameAlreadyExists) {
+        return response.status(400).json({error: "Username already exists"})
+    }
+    users.push({
+      id: uuidv4(),
+      name,
+      username,
+      todos: [],
+    })
+    console.log(users)
+    return response.status(201).json(users)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
